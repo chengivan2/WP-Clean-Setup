411ced1aae9462a8a4aad2d9d17995c7db4bb90a
chengivan <chengoleivan@gmail.com>
2024-01-14 14:38:57 +0300

    Initial commit

---
+++ wpcleansetup.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WP Clean Setup
+Description: A WordPress plugin that deletes posts and plugins on activation.
+Version: 1.0
+Author: Ivan the Dev
+Author URI: https://ivanthedev.guru
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+*/
+
+// Register activation hook
+register_activation_hook( __FILE__, 'wpcs_delete_posts_and_plugins' );
+
+// Define function to delete posts and plugins
+function wpcs_delete_posts_and_plugins() {
+
+    // Delete posts with post id 1 and 2
+    wp_delete_post( 1, true );
+    wp_delete_post( 2, true );
+
+    // Delete plugins akismet and hello dolly
+    delete_plugins( array( 'akismet/akismet.php', 'hello-dolly/hello.php' ) );
+}
